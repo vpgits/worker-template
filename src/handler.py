91a6ff3f5092cc3a5d67b70fff8e87918fa93821
vpgits <103498292+vpgits@users.noreply.github.com>
2023-12-18 09:28:06 +0000

    modified the generator pipeline

--- src/handler.py
+++ src/handler.py
@@ -3,26 +3,30 @@
 import runpod
 import torch
 from auto_gptq import AutoGPTQForCausalLM
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, TextGenerationPipeline
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
+device = "cuda:0"
 
 model = AutoGPTQForCausalLM.from_quantized("/src/Mistral-7B-v0.1-qagen-v0.6-4bit")
 tokenizer = AutoTokenizer.from_pretrained("/src/Mistral-7B-v0.1-qagen-v0.6-4bit", use_fast=True)
 
 
+pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=device)
 
-def generate(prompt):
-    return (tokenizer.decode(model.generate(**tokenizer(prompt, return_tensors="pt").to(model.device))[0]))
+
+def generate(kwargs):
+    return (pipeline(kwargs)[0]["generated_text"])
 
 
 
 def handler(job):
     """ Handler function that will be used to process jobs. """
-    prompt = job['input']
+    params = job['input']
 
-    output = generate(prompt)
+
+    output = generate(params)
 
     return output
 
