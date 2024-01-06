2a260daaa6d51df9fccbfa115291c3ad61a2c500
vpgits <103498292+vpgits@users.noreply.github.com>
2024-01-06 03:36:55 +0000

    added token limit param

--- src/handler.py
+++ src/handler.py
@@ -3,7 +3,7 @@
 import runpod
 import torch
 from auto_gptq import AutoGPTQForCausalLM
-from transformers import AutoTokenizer, TextGenerationPipeline
+from transformers import AutoTokenizer
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
@@ -13,20 +13,22 @@ tokenizer = AutoTokenizer.from_pretrained("/src/Mistral-7B-v0.1-qagen-v0.6-4bit"
 
 
 
-def generate(prompt):
+def generate(prompt, token_limit=512):
     model.eval()
     with torch.no_grad():
         model_input = tokenizer(prompt, return_tensors="pt").to("cuda")
-        return tokenizer.decode(model.generate(**model_input, max_new_tokens=512)[0], skip_special_tokens=True)
+        return tokenizer.decode(model.generate(**model_input, max_new_tokens=token_limit)[0], skip_special_tokens=True)
 
 
 
 def handler(job):
     """ Handler function that will be used to process jobs. """
     prompt = job['input']['text']
+    token_limit = job['input']['token_limit']
 
 
-    output = generate(prompt)
+
+    output = generate(prompt, token_limit)
 
     return output
 
